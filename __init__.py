60f9eb94d15a431c3199eb82e7031eaf384a272d
Andrew Wong <andrew.j.wong@outlook.com>
2024-05-13 20:20:47 +1000

    copy paste find replace

--- __init__.py
+++ __init__.py
@@ -3,12 +3,11 @@ from flask import Blueprint
 from CTFd.models import Challenges, db
 from CTFd.plugins import register_plugin_assets_directory
 from CTFd.plugins.challenges import CHALLENGE_CLASSES, BaseChallenge
-from CTFd.plugins.dynamic_challenges.decay import DECAY_FUNCTIONS, logarithmic
-from CTFd.plugins.migrations import upgrade
+from CTFd.plugins.delayed_result.decay import DECAY_FUNCTIONS, logarithmic
 
 
-class DynamicChallenge(Challenges):
-    __mapper_args__ = {"polymorphic_identity": "dynamic"}
+class DelayedResult(Challenges):
+    __mapper_args__ = {"polymorphic_identity": "delayed"}
     id = db.Column(
         db.Integer, db.ForeignKey("challenges.id", ondelete="CASCADE"), primary_key=True
     )
@@ -18,35 +17,35 @@ class DynamicChallenge(Challenges):
     function = db.Column(db.String(32), default="logarithmic")
 
     def __init__(self, *args, **kwargs):
-        super(DynamicChallenge, self).__init__(**kwargs)
+        super(DelayedResult, self).__init__(**kwargs)
         self.value = kwargs["initial"]
 
 
-class DynamicValueChallenge(BaseChallenge):
-    id = "dynamic"  # Unique identifier used to register challenges
-    name = "dynamic"  # Name of a challenge type
+class DelayedResultChallenge(BaseChallenge):
+    id = "delayed"  # Unique identifier used to register challenges
+    name = "delayed"  # Name of a challenge type
     templates = (
         {  # Handlebars templates used for each aspect of challenge editing & viewing
-            "create": "/plugins/dynamic_challenges/assets/create.html",
-            "update": "/plugins/dynamic_challenges/assets/update.html",
-            "view": "/plugins/dynamic_challenges/assets/view.html",
+            "create": "/plugins/delayed_result/assets/create.html",
+            "update": "/plugins/delayed_result/assets/update.html",
+            "view": "/plugins/delayed_result/assets/view.html",
         }
     )
     scripts = {  # Scripts that are loaded when a template is loaded
-        "create": "/plugins/dynamic_challenges/assets/create.js",
-        "update": "/plugins/dynamic_challenges/assets/update.js",
-        "view": "/plugins/dynamic_challenges/assets/view.js",
+        "create": "/plugins/delayed_result/assets/create.js",
+        "update": "/plugins/delayed_result/assets/update.js",
+        "view": "/plugins/delayed_result/assets/view.js",
     }
     # Route at which files are accessible. This must be registered using register_plugin_assets_directory()
-    route = "/plugins/dynamic_challenges/assets/"
+    route = "/plugins/delayed_result/assets/"
     # Blueprint used to access the static_folder directory.
     blueprint = Blueprint(
-        "dynamic_challenges",
+        "delayed_result",
         __name__,
         template_folder="templates",
         static_folder="assets",
     )
-    challenge_model = DynamicChallenge
+    challenge_model = DelayedResult
 
     @classmethod
     def calculate_value(cls, challenge):
@@ -65,7 +64,7 @@ class DynamicValueChallenge(BaseChallenge):
         :param challenge:
         :return: Challenge object, data dictionary to be returned to the user
         """
-        challenge = DynamicChallenge.query.filter_by(id=challenge.id).first()
+        challenge = DelayedResult.query.filter_by(id=challenge.id).first()
         data = {
             "id": challenge.id,
             "name": challenge.name,
@@ -108,18 +107,20 @@ class DynamicValueChallenge(BaseChallenge):
                 value = float(value)
             setattr(challenge, attr, value)
 
-        return DynamicValueChallenge.calculate_value(challenge)
+        return DelayedResultChallenge
+    .calculate_value(challenge)
 
     @classmethod
     def solve(cls, user, team, challenge, request):
         super().solve(user, team, challenge, request)
 
-        DynamicValueChallenge.calculate_value(challenge)
+        DelayedResultChallenge
+    .calculate_value(challenge)
 
 
 def load(app):
-    upgrade(plugin_name="dynamic_challenges")
-    CHALLENGE_CLASSES["dynamic"] = DynamicValueChallenge
+    CHALLENGE_CLASSES["dynamic"] = DelayedResultChallenge
+
     register_plugin_assets_directory(
-        app, base_path="/plugins/dynamic_challenges/assets/"
+        app, base_path="/plugins/delayed_result/assets/"
     )
